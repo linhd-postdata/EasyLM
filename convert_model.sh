7326e30904eec39f4bdf6295177de5021541f589
Álvaro Pérez <alvaro.perez.pozo@gmail.com>
2023-07-13 12:22:08 +0200

    Fix convert script

--- convert_model.sh
+++ convert_model.sh
@@ -1,6 +1,6 @@
-git lfs install
+git lfs installuggingface-cli login 
 git clone https://huggingface.co/decapoda-research/llama-7b-hf
-python EasyLM/models/llama/convert_hf_to_easylm.py \
+python -m EasyLM.models.llama.convert_hf_to_easylm \
 	--checkpoint_dir llama-7b-hf \
 	--output_file llama_easylm.stream \
 	--model_size 7b \
